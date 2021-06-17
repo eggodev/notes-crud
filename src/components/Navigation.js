cf12e62c881c04e84ba3caca6ee8e27fa30d5340
eggodev <eggodev@gmail.com>
2021-06-17 14:18:36 -0400

    prueba

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -24,7 +24,7 @@ export default class Navigation extends Component {
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/">
-                  Notes
+                  Notesss
                 </Link>
               </li>
               <li className="nav-item">
